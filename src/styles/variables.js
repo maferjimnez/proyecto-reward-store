553794e3c0b4f3dd40d204e1d422eeb34855918f
María Jiménez <jmmariafernanda15@gmail.com>
2020-12-21 22:40:02 -0300

    Agrego variables de estilos

---
+++ src/styles/variables.js
@@ -0,0 +1,10 @@
+const variables = {
+    primaryBLue: '#5edfff',
+    secondayBlue: '#b2fcff',
+    lightBackground: '#ecfcff',
+    gray: '#f1f6f9',
+    white: '#ffff',
+    colorShadow: '#babecc'
+};
+
+export default variables;
