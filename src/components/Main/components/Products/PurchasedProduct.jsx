f1a670621f300bd33e3cc9628909fd34448357aa
María Jiménez <jmmariafernanda15@gmail.com>
2021-02-26 14:52:18 -0300

    Agrego estilos del historial

--- src/components/Main/components/Products/PurchasedProduct.jsx
+++ src/components/Main/components/Products/PurchasedProduct.jsx
@@ -1,15 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function PurchasedProduct(props) {
   return (
-    <div>
-      <img src={props.img.url} alt="Il" />
-      <div>
-        <p>{props.name}</p>
-        <p>{props.cost}</p>
-      </div>
-    </div>
+    <ProductContainer>
+      <ProductImg src={props.img.url} alt="Il" />
+      <ProductInfo>
+        <div>
+          <ProductName>{props.name}</ProductName>
+          <ProductCategory>{props.category}</ProductCategory>
+        </div>
+        <div>
+          <span>Price:</span>
+          <ProductCategory>{props.cost} coins.</ProductCategory>
+        </div>
+      </ProductInfo>
+    </ProductContainer>
   );
 }
+
+const ProductContainer = styled.div`
+  margin-bottom: 1rem;
+  padding: 1rem;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  background-color: #f1f6f9;
+`;
+
+const ProductImg = styled.img`
+  width: 8rem;
+  border-radius: 10px;
+  @media (min-width: 100px) {
+    width: 10rem;
+  }
+`;
+
+const ProductInfo = styled.div`
+  margin-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const ProductName = styled.p`
+  font-weight: 600;
+`;
+
+const ProductCategory = styled.p`
+  font-size: 14px;
+`;
 
 export default PurchasedProduct;
--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react';
+import styled from 'styled-components';
 import { UserContext } from '../components/Context/UserContext';
 import PurchasedProduct from '../components/Main/components/Products/PurchasedProduct';
 import { fetchUserPurchases } from '../services/api';
@@ -12,12 +13,38 @@ function Purchases() {
   }, []);
 
   return (
-    <div>
-      {productsPurchased.map((product) => (
-        <PurchasedProduct {...product} key={product.createDate} />
-      ))}
-    </div>
+    <PurchasesContainer>
+      <h1>Purchases:</h1>
+      <ProductsWrapper>
+        {productsPurchased.map((product) => (
+          <PurchasedProduct {...product} key={product.createDate} />
+        ))}
+      </ProductsWrapper>
+    </PurchasesContainer>
   );
 }
+
+const PurchasesContainer = styled.div`
+  margin: 1rem 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ProductsWrapper = styled.div`
+  width: 100%;
+  margin-top: 1.5rem;
+  @media (min-width: 500px) {
+    width: 80%;
+  }
+
+  @media (min-width: 700px) {
+    width: 60%;
+  }
+
+  @media (min-width: 1000px) {
+    width: 30%;
+  }
+`;
 
 export default Purchases;
